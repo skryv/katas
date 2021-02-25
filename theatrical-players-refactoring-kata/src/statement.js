d43c72bbd57ad64ffe0f4c3eab0d2dbbe68b88d7
Niek Vervaeke <niek.vervaeke@gmail.com>
2021-02-25 15:23:54 +0100

    first refactoring

--- theatrical-players-refactoring-kata/src/statement.js
+++ theatrical-players-refactoring-kata/src/statement.js
@@ -1,43 +1,76 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
 
-function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
+  calculateAmountAndVolumeCreditsAndAlsoAddToResult();
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
 
+  function calculateAmountAndVolumeCreditsAndAlsoAddToResult() {
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-        // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+      const play = plays[perf.playID];
+      let thisAmount = 0;
+      switch (play.type) {
+        case "tragedy":
+          thisAmount += calculateTragedyAmount(perf);
+          volumeCredits += calculateTragedyVolumeCredits(perf);
+
+          break;
+        case "comedy":
+          thisAmount += calculateComedyAmount(perf);
+          volumeCredits += calculateComedyVolumeCredits(perf);
+
+          break;
+        default:
+          throw new Error(`unknown type: ${play.type}`);
+      }
+      // print line for this order
+      result += ` ${play.name}: ${format(thisAmount / 100)} (${
+        perf.audience
+      } seats)\n`;
+      totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
+  }
+
+  function calculateComedyVolumeCredits(perf) {
+    return calculateBaseVolumeCredits(perf) + calculateBonusCredits(perf);
+  }
+
+  function calculateBonusCredits(perf) {
+    return Math.floor(perf.audience / 5);
+  }
+
+  function calculateTragedyVolumeCredits(perf) {
+    return calculateBaseVolumeCredits(perf);
+  }
+
+  function calculateBaseVolumeCredits(perf) {
+    return Math.max(perf.audience - 30, 0);
+  }
+
+  function calculateComedyAmount(perf) {
+    let thisAmount = 30000;
+    if (perf.audience > 20) {
+      thisAmount += 10000 + 500 * (perf.audience - 20);
+    }
+    thisAmount += 300 * perf.audience;
+    return thisAmount;
+  }
+
+  function calculateTragedyAmount(perf) {
+    let thisAmount = 40000;
+    if (perf.audience > 30) {
+      thisAmount += 1000 * (perf.audience - 30);
+    }
+    return thisAmount;
+  }
 }
 
 module.exports = statement;
